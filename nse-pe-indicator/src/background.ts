89ef77387052136e33c5ffa6faa96395ae63a4c5
Sandeep Rawat <noreply@sandeeprawat.com>
2017-02-24 00:51:42 +0530

    1) Update manifest details. 2) Add logic to read Nifty PE 3) Added application icon

--- nse-pe-indicator/src/background.ts
+++ nse-pe-indicator/src/background.ts
@@ -1,19 +1,58 @@
 class peDetails {
-  constructor(value:string){
+  constructor(value: string) {
     this.Value = value;
   }
   Value: string;
 }
 
-class Config{
-  public bgColor:string = "blue"
+class Config {
+  public url : string = "aHR0cHM6Ly9jcmF5dGhlb24uY29tL2NoYXJ0cy9uaWZ0eV9wZV9yYXRpb19wYl92YWx1ZV9kaXZpZGVuZF95aWVsZF9jaGFydC5waHA";
+  public querySelector: string = "div #contact > div > div > div > b";
+  public getColor(pe: string): string
+  {
+    let peratio = parseInt(pe);
+    if(peratio == 0)
+    {
+      return "transparent";
+    }
+    else if(peratio < 16)
+    {
+      return "DarkGreen";
+    }
+    else if(peratio < 20)
+    {
+      return "LightGreen";
+    }
+    else if(peratio < 22)
+    {
+      return "SlateGrey";
+    }
+    else if(peratio < 24)
+    {
+      return "MediumOrchid";
+    }
+    else if(peratio < 28)
+    {
+      return "Red";
+    }
+    else return "DarkRed";
+  }
 }
 
-let pe = new peDetails("24.3");
-chrome.browserAction.setBadgeText({
-  text : pe.Value
-});
 let config = new Config();
-chrome.browserAction.setBadgeBackgroundColor({
-  color:config.bgColor
-});
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", atob(config.url));
+
+xhttp.onreadystatechange = function () {
+  if (xhttp.readyState == 4) {
+    // innerText does not let the attacker inject HTML elements.
+    let div = document.createElement("div");
+    div.innerHTML = xhttp.responseText;
+    let peRatio = div.querySelector(config.querySelector);
+    chrome.browserAction.setBadgeText({ text: peRatio.textContent });
+    chrome.browserAction.setBadgeBackgroundColor({ color: config.getColor(peRatio.textContent)});
+  }
+}
+xhttp.send();
