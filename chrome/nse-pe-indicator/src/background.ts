29d37aa3f55db5e7f225c79bdf5a1e794cf5d342
Sandeep Rawat <noreply@sandeeprawat.com>
2017-03-01 00:23:15 +0530

    Updating edge and chrome sample

--- chrome/nse-pe-indicator/src/background.ts
+++ chrome/nse-pe-indicator/src/background.ts
@@ -1,43 +1,5 @@
-class peDetails {
-  constructor(value: string) {
-    this.Value = value;
-  }
-  Value: string;
-}
 
-class Config {
-  public url : string = "aHR0cHM6Ly9jcmF5dGhlb24uY29tL2NoYXJ0cy9uaWZ0eV9wZV9yYXRpb19wYl92YWx1ZV9kaXZpZGVuZF95aWVsZF9jaGFydC5waHA";
-  public querySelector: string = "div #contact > div > div > div > b";
-  public getColor(pe: string): string
-  {
-    let peratio = parseInt(pe);
-    if(peratio == 0)
-    {
-      return "transparent";
-    }
-    else if(peratio < 16)
-    {
-      return "DarkGreen";
-    }
-    else if(peratio < 20)
-    {
-      return "LightGreen";
-    }
-    else if(peratio < 22)
-    {
-      return "SlateGrey";
-    }
-    else if(peratio < 24)
-    {
-      return "MediumOrchid";
-    }
-    else if(peratio < 28)
-    {
-      return "Red";
-    }
-    else return "DarkRed";
-  }
-}
+import {Config} from "./Config";
 
 let config = new Config();
 
@@ -47,12 +9,10 @@ xhttp.open("GET", atob(config.url));
 
 xhttp.onreadystatechange = function () {
   if (xhttp.readyState == 4) {
-    // innerText does not let the attacker inject HTML elements.
-    let div = document.createElement("div");
-    div.innerHTML = xhttp.responseText;
-    let peRatio = div.querySelector(config.querySelector);
-    chrome.browserAction.setBadgeText({ text: peRatio.textContent });
-    chrome.browserAction.setBadgeBackgroundColor({ color: config.getColor(peRatio.textContent)});
+    var peDetails : any[] = JSON.parse(xhttp.responseText);
+    var peRatio:number = peDetails.filter(pe => pe.RowKey == 'nseLoader')[0].PE;
+    chrome.browserAction.setBadgeText({ text: peRatio.toString() });
+    chrome.browserAction.setBadgeBackgroundColor({ color: config.getColor(peRatio)});
   }
 }
 xhttp.send();
--- edge/nse-pe-indicator/js/background.js
+++ edge/nse-pe-indicator/js/background.js
@@ -1,5 +1,5 @@
+browser.browserAction.setBadgeText({text:"Testing setBadgeText"});
 
-browser.browserAction.setBadgeText({ text: "Testing setBadgeText" });
 browser.browserAction.setBadgeBackgroundColor({ color: [127, 127, 127, 127] });
 browser.browserAction.getBadgeText({}, function (details) {
   console.log(details);
